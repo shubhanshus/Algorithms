350180f1d72e94916d588f49a4fe3cfb29398225
Shubhanshu Surana <shubhanshu.surana@gmail.com>
2018-09-08 20:53:38 -0400

    inital version of Stack

---
+++ src/stack/stack.go
@@ -0,0 +1,34 @@
+package stack
+
+import "sync"
+
+// Item the type of the stack
+type Item interface{}
+
+// Stack the stack of Items
+type Stack struct {
+	items []Item
+	lock  sync.RWMutex
+}
+
+// New  creates a new stack
+func (s *Stack) New() *Stack {
+	s.items = []Item{}
+	return s
+}
+
+// Push puts the item on the stack
+func (s *Stack) Push(t Item) {
+	s.lock.Lock()
+	s.items = append(s.items, t)
+	s.lock.Unlock()
+}
+
+//Pop removes the item from the stack
+func (s *Stack) Pop() *Item {
+	s.lock.Lock()
+	item := s.items[len(s.items)-1]
+	s.items = s.items[0 : len(s.items)-1]
+	s.lock.Unlock()
+	return &item
+}
